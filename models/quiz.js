cad6b9c6542e2ab7a4d8440b8fe1e4746315914a
palarcom <palarcom@gmail.com>
2015-08-02 19:12:08 +0200

    Autoload

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,8 @@
+// Definicion del modelo de Quiz
+
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Quiz',
-     {  pregunta: DataTypes.STRING,
-        respuesta: DataTypes.STRING
-     });
+            { pregunta:  DataTypes.STRING,
+              respuesta: DataTypes.STRING,
+            });
 }
-    
